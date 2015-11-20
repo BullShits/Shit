7ce64595f4e9defe2013ff1bb662edf6029efa42
tile <nguyenngoi9190@gmail.com>
2015-11-20 20:51:54 +0700

    update shit

--- hello.go
+++ hello.go
@@ -5,5 +5,5 @@ import "fmt"
 // You can run go file by command "go run hello.go"
 func main(){
 	/* This is my first sample program.  */
-	fmt.Printf("Hello Golang!")
+	fmt.Printf("Hello Golang!\n")
 }
